05b3caaa6a5579f4d151b1b89d8858ee5e736cda
Rushikesh041 <jadhavrushi041@gmail.com>
2022-10-20 19:57:32 +0530

    Day5

--- Day5Problem/Program.cs
+++ Day5Problem/Program.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-
+            //Calling 
             UserInput Input = new UserInput();
-            Input.flipCoinPercentageProblem(); // Calling Flip Coin and print percentage of Heads and Tails Problem
-            Input.leapYearProblem();           // Calling Leap Year Problem
-            Input.powerOf2Problem();           // Calling Power of 2 Problem
-            Input.harmonicNumberProblem();     // Calling Harmonic Number Problem
-            Input.primeFactorOfNumberProblem(); // Calling Prime Fctors Of Number Problem
-            Input.quotientNdRemainderProblem(); // Calling Compute Quotient and Remainder Problem
-            Input.swapTwoNumbersProblem();      // Calling Swap Two Numbers Problem
-            Input.evenOrOddProblem();           // Calling Check Whether a Number is Even or Odd Problem
-            Input.vowelOrConsonentProblem();    // Calling Check Whether an Alphabet is Vowel or Consonant Problem
-            Input.largestNumberAmongUsProblem(); // Calling Find the Largest Among Three Numbers Problem
+            Input.flipCoinPercentageProblem(); 
+            Input.leapYearProblem();           
+            Input.powerOf2Problem();          
+            Input.harmonicNumberProblem();    
+            Input.primeFactorOfNumberProblem();
+            Input.quotientNdRemainderProblem();
+            Input.swapTwoNumbersProblem();     
+            Input.evenOrOddProblem();          
+            Input.vowelOrConsonentProblem();   
+            Input.LargestNumberProblem(); 
 
         }
     }
